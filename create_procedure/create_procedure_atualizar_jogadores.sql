c146e19c14ab71996dd3f212b3c5d33944b48619
Francieli Silva Abreu <fsilvaab@everis.com>
2020-10-29 18:06:20 -0300

    atualizacao de create e inclusao de inserts

--- create_procedure/create_procedure_atualizar_jogadores.sql
+++ create_procedure/create_procedure_atualizar_jogadores.sql
@@ -1,18 +1,19 @@
-CREATE PROCEDURE atualizar_jogadores_clube
-@Nome1 Varchar(100),
-@ClubeId1 int,
-@Nome2 Varchar(100),
-@ClubeId2 int
+CREATE PROCEDURE SP_Atualizar_Jogador
+
+    @JogadorId INT,
+    @ClubeId INT
+
+
 AS
 BEGIN
-    UPDATE Jogador
-    SET Nome = @Nome1 
-    WHERE ClubeId = @ClubeId1
+    UPDATE
+        Jogador SET 
+        ClubeId = @ClubeId
+        WHERE Id = @JogadorId
 
-    UPDATE Jogador
-    SET Nome = @Nome2
-    WHERE ClubeId = @ClubeId2
 END
 
-EXECUTE atualizar_jogadores_clube 'silvio',3,'fran',1
+
+EXEC SP_Atualizar_Jogador  2,1
+
 SELECT * FROM Jogador
---
+++ insert/add_news_coluna.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Clube
+    ADD Vitorias INT,
+        Derrotas INT,
+        Empates INT,
+        Gols_Pro INT,
+        Gols_Contra INT,
+		Partidas INT,
+        Saldo_de_Gols INT;
